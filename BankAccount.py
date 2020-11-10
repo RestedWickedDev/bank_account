5ab91e335cabde87419d031ebe00bb9171be93c5
RestedWickedDev <jesusdperez@gmail.com>
2020-11-09 18:57:00 -0800

    Update BankAccount.py

--- BankAccount.py
+++ BankAccount.py
@@ -7,12 +7,27 @@ class BankAccount:
 
     ### METHODS
 
-    def deposit
+    def deposit(self, amount):
+        self.balance = self.balance + amount
+        print("Amount Deposited: $" + str(self.balance))
 
-    def withdraw
+    def withdraw(self, amount):
+        if self.balance > amount:
+            self.balance = self.balance - amount
+            print("Amount Withdrawn: $" + str(self.balance))
+        else:
+            print("Insufficient funds")
 
-    def get_balance
+    def get_balance(self):
+        print("Your balance is: $" + str(self.balance))
 
-    def add_interest
+    def add_interest(self):
+        interest = self.balance *  0.00083 
+        self.balance = self.balance + interest
 
-    def print_receipt
+    def print_receipt(self):
+        print(self.full_name)
+        print(self.account_number)
+        print(self.routing_number)
+        print(self.balance)
+
